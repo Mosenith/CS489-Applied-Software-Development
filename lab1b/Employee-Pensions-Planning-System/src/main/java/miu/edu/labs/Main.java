17571438b49c16ae541e17788e2a7e762305eaa3
MosenithHeang <heang.mosenith.1206@gmail.com>
2024-04-03 00:54:26 -0500

    Lab1b: task1 & task2

--- lab1b/Employee-Pensions-Planning-System/src/main/java/miu/edu/labs/Main.java
+++ lab1b/Employee-Pensions-Planning-System/src/main/java/miu/edu/labs/Main.java
@@ -1,7 +1,84 @@
 package miu.edu.labs;
 
+import miu.edu.labs.model.Employee;
+import miu.edu.labs.model.PensionPlan;
+
+import javax.management.Query;
+import java.time.LocalDate;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee e1 = new Employee(1L, "Daniel", "Agar", LocalDate.of(2018,01,17), 105945.50);
+        Employee e2 = new Employee(2L, "Bernard", "Shaw", LocalDate.of(2019,04,04), 197750.00);
+        Employee e3 = new Employee(3L, "Carly", "Agar", LocalDate.of(2014,05,16), 842000.75);
+        Employee e4 = new Employee(4L, "Wesley", "Schneider", LocalDate.of(2019,05,02), 74500.00);
+
+        PensionPlan p1 = new PensionPlan("EX1089", null, 100);
+        PensionPlan p2 = new PensionPlan("SM2307", null, 1555.50);
+
+        Queue<PensionPlan> pensionPlanQueue = new ArrayDeque<>();
+        pensionPlanQueue.add(p1);
+        pensionPlanQueue.add(p2);
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(e4);
+        employeeList.add(e3);
+        employeeList.add(e1);
+        employeeList.add(e2);
+
+        employeeList.sort(Comparator.comparing(Employee::getLastName).thenComparing(Employee::getYearlySalary, Comparator.reverseOrder()));
+
+        // For JSON format
+        System.out.println("Printed in JSON format\n[");
+        for(Employee employee : employeeList) {
+            if(isQualifiedForEnrollment(employee)) {
+                if(!pensionPlanQueue.isEmpty()) {
+                    employee.setPensionPlan(pensionPlanQueue.poll(),employee);
+                }
+            }
+            System.out.println(employee.toJSONString());
+        }
+
+        System.out.println("]");
+        System.out.println("------------------------------");
+
+        // Task 2
+        System.out.println("Printed in JSON format\n[");
+        for(Employee employee : employeeList) {
+            if(employee.getPensionPlan() == null && isQualifiedNextMonth(employee)) {
+                System.out.println(employee.toJSONString());
+            }
+        }
+        System.out.println("]");
+    }
+
+
+
+    private static boolean isQualifiedForEnrollment(Employee employee) {
+        LocalDate today = LocalDate.now();
+        LocalDate startDate = employee.getEmploymentDate();
+        if(startDate.plusYears(5).isBefore(today)) { // more than 5 years
+            return true;
+        }
+
+
+        return false;
+    }
+
+    private static boolean isQualifiedNextMonth(Employee employee) {
+        LocalDate today = LocalDate.now();
+        LocalDate firstDayOfNextMonth = today.plusMonths(1).withDayOfMonth(1);;
+        LocalDate lastDayOfNextMonth = firstDayOfNextMonth.plusMonths(1).minusDays(1);
+        LocalDate startDate = employee.getEmploymentDate();
+
+        // will be qualify next month : on or between 1st & last day of next month
+        LocalDate nextFiveYears = startDate.plusYears(5);
+        if(nextFiveYears.isEqual(firstDayOfNextMonth) || nextFiveYears.isEqual(lastDayOfNextMonth) ||
+                nextFiveYears.isAfter(firstDayOfNextMonth) && nextFiveYears.isBefore(lastDayOfNextMonth)) {
+            return true;
+        }
+
+        return false;
     }
 }
--- lab1b/Employee-Pensions-Planning-System/src/main/java/miu/edu/labs/model/Employee.java
+++ lab1b/Employee-Pensions-Planning-System/src/main/java/miu/edu/labs/model/Employee.java
@@ -8,4 +8,85 @@ public class Employee {
     private String lastName;
     private LocalDate employmentDate;
     private double yearlySalary;
+
+    private PensionPlan pensionPlan;
+
+    public Employee(long employeeId, String firstName, String lastName, LocalDate employmentDate, double yearlySalary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employmentDate = employmentDate;
+        this.yearlySalary = yearlySalary;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getEmploymentDate() {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(LocalDate employmentDate) {
+        this.employmentDate = employmentDate;
+    }
+
+    public double getYearlySalary() {
+        return yearlySalary;
+    }
+
+    public void setYearlySalary(double yearlySalary) {
+        this.yearlySalary = yearlySalary;
+    }
+
+    public PensionPlan getPensionPlan() {
+        return pensionPlan;
+    }
+
+    public void setPensionPlan(PensionPlan pensionPlan) {
+        this.pensionPlan = pensionPlan;
+    }
+
+    public void setPensionPlan(PensionPlan pensionPlan, Employee employee) {
+        this.pensionPlan = pensionPlan;
+        pensionPlan.setEnrollmentDate(employee.getEmploymentDate().plusYears(5));
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employmentDate=" + employmentDate +
+                ", yearlySalary=" + yearlySalary +
+                ", pensionPlan=" + pensionPlan +
+                '}';
+    }
+
+    public String toJSONString() {
+        return String.format("\t{\"employeeId\":\"%d\", \"firstName\":\"%s\", \"lastName\":\"%s\", " +
+                        "\"employmentDate\":\"%s\", \"yearlySalary\":%.2f, \"pensionPlan\":%s}",
+                employeeId, firstName, lastName, employmentDate, yearlySalary, pensionPlan);
+    }
 }
